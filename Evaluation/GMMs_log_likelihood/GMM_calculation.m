20fa5ddd2256dd9cc42ff131bebcc878a418056d
Sharaj Panwar <ijz121@my.utsa.edu>
2020-03-11 15:53:19 -0500

    Add files via upload

---
+++ Evaluation/GMMs_log_likelihood/GMM_calculation.m
@@ -0,0 +1,71 @@
+% This script calculates the GMM likelihood of real samples, vanilla GAN generated samples,
+% and CC-GAN generated samples for all 64 channels
+
+% The procedure/code is same for Target and nonTarget samples. 
+% Let's assume Target data
+
+% Upload real target, vanilla GAN generated target, and CC-GAN generated
+% target data. 
+
+% In first, we will identify the number of fitted GMMs for all 64 channels of
+% real target data and store them in gmm_count
+
+gmm_count = zeros(1,64); %create an array to store 
+
+for ch = 1:64;
+    X= squeeze(T_ch64_s1_r2(:,ch,:));
+%determining the no of GMMs using BCI method 
+options = statset('Display','final');
+GMModel = fitgmdist(X,2,'CovarianceType','diagonal','Options',options, 'RegularizationValue',0.1);
+
+% Examine the BIC over varying numbers of components k, here we check for maximum 10 GMM component.
+BIC = zeros(1,10);
+GMModels = cell(1,10);
+options = statset('MaxIter',500);
+for k = 1:10;
+    GMModels{k} = fitgmdist(X,k,'Options',options,'CovarianceType','diagonal','RegularizationValue',0.1);
+    BIC(k)= GMModels{k}.BIC;
+end
+
+[minBIC,numComponents] = min(BIC);
+numComponents ;% out put the number of GMM component fitted 
+gmm_count(ch) = numComponents;
+end
+
+% Create an array of 64x4 to store no of fitted GMM, gmm likelihood of samples from real,
+% Vanilla GAN, and CC_GAN 0f 64 channels
+
+GMM_likelihood = zeros(64,4);
+% we will use "GMM_likelihood_channel" function to calculate The GMM
+% likelihood distance for each channel iteratively and store in
+% "GMM_likelihood" array, please refer to the function script
+% GMM_likelihood_channel.m
+
+for n_ch = 1:64;
+    GMM_likelihood_ch = GMM_likelihood_channel(T_ch64_s1_r2, target_gen, target_gen, n_ch, gmm_count);
+    GMM_likelihood(n_ch,:) = GMM_likelihood_ch;
+end
+
+% Now you can have GMM_likelihood array in tabular
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Evaluation/GMMs_log_likelihood/GMM_likelihood_channel.m
@@ -0,0 +1,37 @@
+
+% This function calculate the GMM likelihood of real samples, vanilla GAN generated samples,
+% and CC-GAN generated samples of any given channel
+
+% The GMMs are fitted on real data and average likelihood of real, vanilla
+% GAN and CC-GAN is calculated to compare the sample's quality.
+ 
+
+function GMM_likelihood_channel = Distance(real, Vanilla_GAN, CC_GAN, channel, gmm_count);
+% real => real samples, Vanilla_GAN => Samples generated from vanilla GAN, CC_GAN => samples
+% generated from CC-GAN, channel => channel to be evaluated, i.e. 30 for POz, 
+% gmm_count => no of fitted GMM using BCI (please refer to GMM_calculation.m) 
+real_channel= squeeze(real(:,channel,:));
+Vanila_GAN_channel= squeeze(Vanilla_GAN(:,channel,:));
+CC_GAN_channel= squeeze(CC_GAN(:,channel,:));
+numComponents = gmm_count(channel);
+
+gm =  fitgmdist(real_channel,numComponents,'RegularizationValue',0.1);
+mu = gm.mu; sigma=gm.Sigma;
+p = gm.ComponentProportion;
+
+for i = 1:numComponents;
+    d_real(:,i)=mvnpdf(real_channel, mu(i,:),sigma(:,:,i));
+    d_Vanilla_GAN(:,i)=mvnpdf(Vanila_GAN_channel, mu(i,:),sigma(:,:,i));
+    d_CC_GAN(:,i)=mvnpdf(CC_GAN_channel, mu(i,:),sigma(:,:,i));
+
+end
+% weighing wih the component proportion 
+distance_Real = d_real*p';distance_Vanila_GAN = d_Vanilla_GAN*p';distance_CC_GAN = d_CC_GAN*p';
+GMM_likelihood_Real = log(mean(distance_Real, 1));
+GMM_likelihood_Vanilla_GAN = log(mean(distance_Vanila_GAN, 1));
+GMM_likelihood_CC_GAN = log(mean(distance_CC_GAN, 1));
+
+GMM_likelihood_channel = [numComponents, GMM_likelihood_Real, GMM_likelihood_Vanilla_GAN, GMM_likelihood_CC_GAN];
+end
+% function outputs a 1x4 array with no of fitted GMM, gmm likelihood of samples from real,
+% Vanilla GAN, and CC_GAN 
