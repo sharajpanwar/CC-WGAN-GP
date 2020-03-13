a0649e8f1723dfa84df6dd12ba188ff0be016cbc
Sharaj Panwar <ijz121@my.utsa.edu>
2020-03-12 20:33:46 -0500

    Add files via upload

---
+++ data_processing/channel_wise_data.m
@@ -0,0 +1,26 @@
+
+% This script is to get the channel names and channelwise alerts and drowsy data 
+% Loading the data files 
+load('dir/Target');
+load('dir/nonTarget');
+load('dir/chanlocs.mat');
+
+% creating a table of 64 channel names, this is useful for writeup too
+ 
+channels = struct2cell(chanlocs64);
+channels_names = channels(1,1,:)
+channels_names=squeeze(channels_names);
+channel_names_table =  cell2table(channels_names)
+
+%Now creating a cell of channel wise alerts (row1) and drowsy (row2)
+
+channel_data  = cell(1,64);
+
+ 
+for i=1:64  
+  channel_data{1,i}  = Target(:,i,:);
+  channel_data{2,i}  = nonTarget(:,i,:);   
+end
+ 
+% these channels can be used to fit channel wise Support Vector Machine and
+% Accuracy can be used as a measure to order them in terms of discriminative features
