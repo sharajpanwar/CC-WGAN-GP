521c21e4b8343548d8ba63c4c115392b78e6790c
Sharaj Panwar <ijz121@my.utsa.edu>
2020-03-14 13:40:18 -0500

    Add files via upload

---
+++ deep_classifiers/data_import.py
@@ -0,0 +1,15 @@
+
+import keras
+from random import shuffle
+import os
+os.environ["CUDA_VISIBLE_DEVICES"]= '0'
+Channels, time_step = 64, 64
+
+def data_import(X, Y):
+    ind_list = [i for i in range(X.shape[0])]; shuffle(ind_list)
+    X = X[ind_list, :,:];    Y = Y[ind_list]
+    #the EEGNet code is written for keras defaul settings: "image_data_format": "channels_last"
+    X = X.reshape(X.shape[0], Channels, time_step, 1) #reshape the data
+    Y[Y==4]=0;    Y[Y==5]=1
+    Y = keras.utils.to_categorical(Y, 2) #one hot encoding of labels
+    return X, Y
