b7ec721d64f7f4ad5eead1c9d9f7081e6c01f34a
Sharaj Panwar <ijz121@my.utsa.edu>
2020-04-04 19:52:32 -0500

    Update HeatMap.m

--- visual_inspection/HeatMap.m
+++ visual_inspection/HeatMap.m
@@ -4,9 +4,11 @@
 load('dir/Target_gen')
 % average the samples and create 2D array of (channel, timestep => 64 x 64)
 Target_avg= squeeze(mean(target_gen(:, :, :), 1));
-% 
+% plot(HeatMap(Target_avg),'color',[0 0.4470 0.7410],'LineWidth',2, 'res', 300);
 figure
-plot(HeatMap(Target_avg),'color',[0 0.4470 0.7410],'LineWidth',2, 'res', 300);
+M3= squeeze(mean(T_ch64_s1_r2(10, :, :), 1));
+H_alerts3 = HeatMap(M3)
+hFig3 = plot(H_alerts3);
 title('Heat_map','FontSize', 24)
 xlabel('Time Steps','FontSize', 24)
 ylabel('Potential','FontSize', 24)
